99798686a0ea7e7a25ba368d89b74af1d31884e7
Brunno Mattos <brunno.mattos@yahoo.com.br>
2016-07-19 23:33:09 -0300

    Fetching credit card data cetelem's site from specific user

--- main.js
+++ main.js
@@ -1,26 +1,69 @@
 var casper = require('casper').create();
+var userName = casper.cli.get('user') + '';
+var password = casper.cli.get('pass') + '';
 
-casper.on('remote.message', function(msg) {
+//require("utils").dump(casper.cli);
+
+casper.on('remote.message', function(msg){
 	this.echo(msg);
 });
 
+
 casper.start('http://www.cetelem.com.br', function() {
-	this.capture('./output/test.png');
-	console.log('Testing...');
-	var user = this.getElementAttribute('#usuario', 'value');
-	console.log('User before: ' + user);
-	//casper.sendKeys('#usuario', 'teste');
+	this.capture('./output/Step1.png');
+	console.log('### Step1: Fill username ###');
+
 	this.fillSelectors('form[method="get"]', {
-		'input[name="usuario"]': 'teste'
+		'input[name="usuario"]': userName
 	}, true);
-	user = this.getElementAttribute('#usuario', 'value');
-	console.log('User after: ' + user);
-	//this.click('#button');
 });
 
 casper.waitForSelector('input#password', function() {
-	console.log('next page...');
-	this.capture('./output/test2.png');
+	console.log('### Step2: Fill password ###');
+	this.capture('./output/Step2.png');
+	
+
+	for (var i = 0; i < password.length; i++)
+	{
+		this.click('#tecladoVirtual table tr td input[value="'+ password[i] +'"]');
+	}
+
+	this.capture('./output/Step2_1.png');	
+
+	console.log('### Step3: Enter account overview page ###');
+	
+	this.click('#acessarBt img');
+	this.capture('./output/Step3.png');
 });
 
-casper.run();
+casper.waitForSelector('.destaqueVerde', function(){
+	console.log('### Step4: Evaluate credit card information ###');
+	this.capture('./output/Step4.png');
+
+	var data = this.evaluate(function(){
+		var result = [];
+		var limitData = document.querySelectorAll('#dadosCartaoInternetBanking span');
+		var creditCardData = document.querySelectorAll('#dadosCartaoInternetBanking2 span');
+		
+		for (var i = 1; i < limitData.length; i++){
+			var obj = {};
+			obj[limitData[i].innerText] = limitData[++i].innerText;
+			result.push(obj);
+		}
+
+		for (var i = 1; i < creditCardData.length; i++){
+			var obj = {};
+			obj[creditCardData[i].innerText] = creditCardData[++i].innerText;
+			result.push(obj);	
+		}
+
+		return result;
+	});
+	
+	console.log('--- Credit Card Informations JSON Data ---');
+	console.log(JSON.stringify(data));
+});
+
+casper.run(function() {
+    this.echo('### Done ###').exit();
+});
